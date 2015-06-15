a8dd72ba58137ff8a4811047a2738b802067fb7e
Apoorva Jain <apoorva.jain@imaginea.com>
2015-06-15 19:00:28 +0530

    Changed data adapter to Restful

--- js/application.js
+++ js/application.js
@@ -1,3 +1,7 @@
 window.Todos = Ember.Application.create();
 
-Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+//Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Todos.ApplicationAdapter = DS.RESTAdapter.extend({
+  host: 'http://localhost:9090/TodoAssignmentAPI'
+});
