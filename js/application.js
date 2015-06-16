9af01f52ada7f6a0b968b5ffca47227b3e0eeb8d
Apoorva Jain <apoorva.jain@imaginea.com>
2015-06-16 18:57:07 +0530

    API interactions

--- js/application.js
+++ js/application.js
@@ -3,5 +3,6 @@ window.Todos = Ember.Application.create();
 //Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 Todos.ApplicationAdapter = DS.RESTAdapter.extend({
-  host: 'http://localhost:9090/TodoAssignmentAPI'
+  host: 'http://localhost:9090/TodoAssignmentAPI',
+  corsWithCredentials: true
 });
--- js/models/todo.js
+++ js/models/todo.js
@@ -1,26 +1,42 @@
 Todos.Todo = DS.Model.extend({
   title: DS.attr('string'),
-  isCompleted: DS.attr('boolean'),
-  dueDate: DS.attr()
+  dueDate: DS.attr('string')
 });
 
-Todos.Todo.FIXTURES = [
- {
-   id: 1,
-   title: 'Learn Ember.js',
-   isCompleted: true,
-   dueDate: '12-May-2015'
- },
- {
-   id: 2,
-   title: '...',
-   isCompleted: false,
-   dueDate: '12-May-2015'
- },
- {
-   id: 3,
-   title: 'Profit!',
-   isCompleted: false,
-   dueDate: '12-May-2015'
- }
-];
+// Todos.Todo.FIXTURES = [
+//  {
+//    id: 1,
+//    title: 'Learn Ember.js',
+//    isCompleted: true,
+//    dueDate: '12-May-2015'
+//  },
+//  {
+//    id: 2,
+//    title: '...',
+//    isCompleted: false,
+//    dueDate: '12-May-2015'
+//  },
+//  {
+//    id: 3,
+//    title: 'Profit!',
+//    isCompleted: false,
+//    dueDate: '12-May-2015'
+//  }
+// ];
+
+Todos.TodoSerializer = DS.RESTSerializer.extend({
+  extractArray : function(store,primaryType,payload){
+    payload = {todos: payload};
+    return this._super(store,primaryType,payload);
+  },
+  extractSingle : function(store,primaryType,payload,recordId){
+    payload = {todo: payload};
+    this._super(store,primaryType,payload,recordId);
+  },
+  serializeIntoHash: function(hash,type,snapshot,options){
+    var json = this.serialize(snapshot,{includeId : true});
+    Object.keys(json).forEach(function(key){
+      hash[key] = json[key]
+    });
+  }
+});
